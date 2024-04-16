c1712bb49e8d2917673f6d3c1b07dd00e821095c
NelsonRamonUFS <70174587+NelsonRamonUFS@users.noreply.github.com>
2024-04-16 20:48:14 -0300

    Soma simples de dois inteiros

---
+++ soma_simples.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main (){
+    
+    int A = 0, B = 0, soma = 0;
+
+    scanf("%d\n", &A);
+    scanf("%d", &B);
+
+    soma = A + B;
+
+    printf("SOMA = %d", soma);
+
+    return 0;
+}
